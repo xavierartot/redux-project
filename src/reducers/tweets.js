9826d0eead3d15c469cfe0a18abdc8813eba9445
xavierartot <xavierartot@gmail.com>
2018-07-22 19:41:56 -0500

    last one

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -2,7 +2,7 @@ import { RECEIVE_TWEETS } from '../actions/tweets';
 
 export default function tweets(state = {}, action) {
   switch (action.type) {
-    case 'RECEIVE_TWEETS':
+    case RECEIVE_TWEETS:
       return {
         ...state, //empty object
         ...action.tweets, //merge all the tweets in this object
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,11 +1,11 @@
-import { RECEIVE_USER } from '../actions/users';
+import { RECEIVE_USERS } from '../actions/users';
 
 export default function users(state = {}, action) {
   switch (action.type) {
-    case 'RECEIVE_USER':
+    case RECEIVE_USERS:
       return {
         ...state, //empty object
-        ...action.users,//merge all the tweets in this object
+        ...action.users, //merge all the tweets in this object
       }
     default:
       return state
