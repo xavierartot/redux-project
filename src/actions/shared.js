e213ce4ee5046aa9db6dc7ddf6603d7c36023a0a
xav <xavierartot@gmail.com>
2018-07-26 14:38:54 -0500

    BUGS: loading bar didn't display

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,18 +2,22 @@ import { getInitialData } from '../utils/api'
 import { receiveTweets } from '../actions/tweets'
 import { receiveUsers } from '../actions/users'
 import { setAuthedUser } from '../actions/authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 // authentification is hard coded
 const AUTHED_ID = 'tylermcginnis'
 
-export function handleInitialData() {
-  return (dispatch, getState) => // thunk pattern with redux-thunk
+export function handleInitialData() { // middleware
+  return (dispatch, getState) => { // thunk pattern with redux-thunk
     getInitialData() // return a promise
       .then(({ users, tweets }) => {
+        dispatch(showLoading()) // show the loading bar
         // promise which will pass to us an object with users and tweets properties
         // let's add users, tweets to the redux store
         dispatch(receiveTweets(tweets))
         dispatch(receiveUsers(users))
         dispatch(setAuthedUser(AUTHED_ID))
+        dispatch(hideLoading()) // hide the loading bar
       })
+  }
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,11 @@
 export function formatDate(timestamp) {
   const d = new Date(timestamp)
   const time = d.toLocaleTimeString('en-US')
+  // time.substr(0, 5) + time.slice(-2) :retire les second et d.toLocaleDateString(): affiche la date
   return `${time.substr(0, 5) + time.slice(-2)} | ${d.toLocaleDateString()}`
 }
 
+// reconstruit les données venant de la DB
 export function formatTweet(tweet, author, authedUser, parentTweet) {
   const {
     id, likes, replies, text, timestamp,
